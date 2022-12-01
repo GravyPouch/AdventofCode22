66328533c1b6501178e36af43955808b69038979
GravyPouch <29843812+GravyPouch@users.noreply.github.com>
2022-12-01 13:28:08 -0500

    Finished 1st day

--- 1st/run.py
+++ 1st/run.py
@@ -1,2 +1,30 @@
-f = open('./data.txt', 'r')
-print(f)
+f = open('data.txt', 'r')
+
+count = 0
+
+elfs = []
+
+elfTotal = 0
+
+while True:
+    count += 1
+
+    line = f.readline()
+
+    if not line:
+        break
+    currentLine = line.strip()
+    if  currentLine == "":
+        elfs.append(elfTotal)
+        print("Total: ",elfTotal)
+        elfTotal = 0
+    else:
+        elfTotal += int(line)
+       
+f.close()
+
+elfs.sort()
+
+print("The most caloric Elf: ",elfs[-1])
+
+print("The top 3 most caloric Elfs: ",elfs[-1] + elfs[-2] + elfs[-3] )
